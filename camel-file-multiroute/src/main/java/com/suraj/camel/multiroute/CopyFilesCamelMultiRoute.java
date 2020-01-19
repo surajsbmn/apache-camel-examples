8eab0616936795e56fce7828efe7ac9f1c377bf4
Suraj Subramanian <surajsbmn@gmail.com>
2020-01-19 14:42:17 +0530

    Camel file multiroute

--- camel-file-multiroute/src/main/java/com/suraj/camel/multiroute/CopyFilesCamelMultiRoute.java
+++ camel-file-multiroute/src/main/java/com/suraj/camel/multiroute/CopyFilesCamelMultiRoute.java
@@ -1,28 +1,30 @@
-package com.suraj.camel.file;
+package com.suraj.camel.multiroute;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 
-public class CopyFilesCamel {
-
+public class CopyFilesCamelMultiRoute {
 	public static void main(String[] args) {
 
 		// Create the Camel context
-		try (CamelContext context = new DefaultCamelContext()){
-			
+		try (CamelContext context = new DefaultCamelContext()) {
+
 			context.addRoutes(new RouteBuilder() {
 				@Override
 				public void configure() throws Exception {
 					// Camel route to copy files from data/input directory to data/output directory
-					from("file:data/input?noop=true").to("log:?level=INFO&showBody=true&showHeaders=true")
-							.to("file:data/output");
+					from("file:data/input_1?noop=true").to("log:?level=INFO&showBody=true&showHeaders=true")
+							.to("file:data/output_1");
+
+					from("file:data/input_2?noop=true").to("log:?level=INFO&showBody=true&showHeaders=true")
+							.to("file:data/output_2").to("file:data/output_3");
 				}
 			});
 
 			// Start the Camel context
 			context.start();
-			
+
 			// Wait for camel to finish the operation
 			Thread.sleep(5000);
 
